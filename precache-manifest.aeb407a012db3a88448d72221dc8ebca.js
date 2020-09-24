5cd033cd662dd5290c16ab32407a5b0e9b989f5e
Mirko <mirko.passoli@gmail.com>
2020-09-24 13:02:21 +0200

    Updates

---
+++ precache-manifest.aeb407a012db3a88448d72221dc8ebca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4937b8ca3ce64f18cba142327bde96b5",
+    "url": "/OrarioUni/index.html"
+  },
+  {
+    "revision": "b304fd5a471bbd2bad1c",
+    "url": "/OrarioUni/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "012176082244264c0a52",
+    "url": "/OrarioUni/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b304fd5a471bbd2bad1c",
+    "url": "/OrarioUni/static/js/2.b12fce4d.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/OrarioUni/static/js/2.b12fce4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "012176082244264c0a52",
+    "url": "/OrarioUni/static/js/main.c37b64e8.chunk.js"
+  },
+  {
+    "revision": "9f42f9aa979100c466b1",
+    "url": "/OrarioUni/static/js/runtime-main.1b4e9523.js"
+  }
+]);
